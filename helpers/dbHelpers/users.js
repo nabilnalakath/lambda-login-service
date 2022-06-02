b173c9dfd971d22694c9cb809f43f5415f0ddee2
nabilnalakath <nabilnalakath@Nabils-MacBook-Pro.local>
2022-06-02 21:03:00 +0530

    Added comments

--- helpers/dbHelpers/users.js
+++ helpers/dbHelpers/users.js
@@ -1,7 +1,10 @@
+// Load the AWS SDK for Node.js
 const AWS = require("aws-sdk");
 
+// Set the region
 AWS.config.update({ region: "us-east-1" });
 
+// Create DynamoDB document client
 const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" });
 
 const userTable = "user-table";
